c5c314bdb59c6c68f84dae4048fdab96edac3150
SuperTikuwa <tikuwamk2@gmail.com>
2023-10-31 23:16:04 +0900

    add: nvim

---
+++ nvim/lua/packers.lua
@@ -0,0 +1,32 @@
+local packer
+local function init( ... )
+  if not packer then
+    packer = require "packer"
+    packer.init { ... }
+  end
+  packer.reset()
+
+  packer.use{
+    -- ここでいろんなプラグインを読み込む
+
+    { "neoclide/coc.nvim" },
+    { "tpope/vim-fugitive" },
+    { "vim-jp/vimdoc-ja" },
+    {
+      "dhruvasagar/vim-table-mode",
+      setup = function()
+        vim.g.table_mode_corner = "|"
+      end,
+    },
+
+    ...
+  }
+end
+
+return setmetatable({}, {
+  __index = function(_, key)
+    init()
+    return packer[key]
+  end,
+})
+
